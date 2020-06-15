c98e80b1212165c052ff245bc6c4f58086906824
Rellyson <rellysondouglas2015@gmail.com>
2020-06-15 09:06:09 -0300

    promisses class

--- testes.js
+++ testes.js
@@ -1,55 +1,26 @@
-var listElement = document.querySelector('#app ul')
-var inputElement = document.querySelector('#app input')
-var btnElement = document.querySelector('#app button')
+var minhaPromise = function() {
+    return new Promise( function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
 
-var todos = JSON.parse(localStorage.getItem('list_to_do')) || []
+        xhr.open('GET', 'https://api.github.com/users/rellyso')
+        xhr.send(null)
 
-function renderTodos() {
-    listElement.innerHTML = ''
-    
-    for (todo of todos) {
-        var createElement = document.createElement('li')
-        var createText = document.createTextNode(todo)
-
-        var linkElement = document.createElement('a')
-        var textLink = document.createTextNode('Excluir')
-
-        var pos = todos.indexOf(todo)
-
-        linkElement.setAttribute('onclick', 'removeTodo('+ pos +')')
-
-
-        linkElement.setAttribute('href', '#')
-        linkElement.appendChild(textLink)
-
-        createElement.appendChild(createText)
-        createElement.appendChild(linkElement)
-        listElement.appendChild(createElement)
-    }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status == 200) {
+                    resolve(JSON.parse(xhr.responseText))
+                } else {
+                    reject('Erro na requisição')
+                }
+            }
+        }
+    })
 }
 
-renderTodos()
-
-
-btnElement.onclick = addTodo
-
-function addTodo() {
-    var inputText = inputElement.value
-
-    todos.push(inputText)
-    console.log(todos)
-
-    inputElement.value = ''
-    renderTodos()
-    saveLocalStorage()
-}
-
-function removeTodo(pos) {
-    todos.splice(pos, 1)
-    renderTodos()
-    saveLocalStorage()
-}
-
-function saveLocalStorage() {
-    localStorage.setItem('list_to_do', JSON.stringify(todos))
-}
+minhaPromise()
+   .then(function(response){
+        console.log(response)
+   })
+   .catch(function(error){
+        console.warn(error)
+   })
